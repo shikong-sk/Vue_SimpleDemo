a560832cdab4e784d73b1350212b99e22f9214a9
shikong-sk <shikong-sk@users.noreply.github.com>
2020-06-21 21:17:00 +0800

    更新

--- src/main.js
+++ src/main.js
@@ -71,6 +71,14 @@ const store = new Vuex.Store({
     },
 })
 
+/**
+ * 引入 Element-UI 组件
+ */
+import ElementUI  from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI)
+
 import App from './App.vue';
 
 new Vue({
---
+++ src/utils/TimeUtils.js
@@ -0,0 +1,18 @@
+/**
+ * 时间工具类
+ */
+const TimeUtils =
+{
+    getDateTime(unixTime) {
+        unixTime = new Date(unixTime)
+        let Y = unixTime.getFullYear()
+        let M = (unixTime.getMonth() + 1) < 10 ? '0' + (unixTime.getMonth() + 1) : (unixTime.getMonth() + 1)
+        let D = unixTime.getDate() < 10 ? '0' + unixTime.getDate() : unixTime.getDate()
+        let H = unixTime.getHours() < 10 ? '0' + unixTime.getHours() : unixTime.getHours()
+        let i = unixTime.getMinutes() < 10 ? '0' + unixTime.getMinutes() : unixTime.getMinutes()
+        let S = unixTime.getSeconds() < 10 ? '0' + unixTime.getSeconds() : unixTime.getSeconds()
+        return Y + '-' + M + '-' + D + ' ' + H + ':' + i + ':' + S
+    }
+}
+
+export default TimeUtils
