acf112e8308b2a1a3fcf1f822b77d5011b4b4b3a
shikong-sk <shikong-sk@users.noreply.github.com>
2020-06-26 14:02:58 +0800

    更新百度地图 工具类

--- src/components/BMap/index.vue
+++ src/components/BMap/index.vue
@@ -20,6 +20,16 @@
 
             this.Map.centerAndZoom(new this.BMap.Point(116.404, 39.915), 11)
 
+            let geolocation = new this.BMap.Geolocation();
+            geolocation.getCurrentPosition(function(r){
+                console.log(this,r);
+                if(this.getStatus() === BMapUtils.BMAP_STATUS_SUCCESS)
+                {
+                    _this.Map.addOverlay(new _this.BMap.Marker(r.point))
+                    _this.Map.panTo(r.point)
+                }
+            })
+
             this.Map.enableDragging()
             this.Map.enableScrollWheelZoom()
         },
