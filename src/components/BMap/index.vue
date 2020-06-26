9aa3a6310c13a74867125bff3460b44371f89940
shikong-sk <shikong-sk@users.noreply.github.com>
2020-06-26 19:02:20 +0800

    更新百度地图 工具类

--- src/components/BMap/index.vue
+++ src/components/BMap/index.vue
@@ -5,6 +5,7 @@
 
 <script>
     import BMapUtils from "@/utils/BMapUtils";
+    import StatusCode from "@/utils/BMapUtils/StatusCode";
 
     export default {
         async mounted() {
@@ -21,12 +22,19 @@
             this.Map.centerAndZoom(new this.BMap.Point(116.404, 39.915), 11)
 
             let geolocation = new this.BMap.Geolocation();
+            geolocation.enableSDKLocation();
             geolocation.getCurrentPosition(function(r){
                 console.log(this,r);
-                if(this.getStatus() === BMapUtils.BMAP_STATUS_SUCCESS)
+                if(this.getStatus() === StatusCode.BMAP_STATUS_SUCCESS)
                 {
-                    _this.Map.addOverlay(new _this.BMap.Marker(r.point))
+                    let marker = new _this.BMap.Marker(r.point,{
+                        title:"当前位置"
+                    })
+                    _this.Map.addOverlay(marker)
+
                     _this.Map.panTo(r.point)
+
+                    _this.Map.setZoom(18)
                 }
             })
 
---
+++ src/utils/BMapUtils/Marker.js
@@ -0,0 +1,13 @@
+/**
+ * 此类表示地图上一个图像标注。
+ */
+export default class Marker {
+    /**
+     * 创建一个图像标注实例。point参数指定了图像标注所在的地理位置
+     * @param point 创建一个图像标注实例。point参数指定了图像标注所在的地理位置
+     * @param opts MarkerOptions
+     */
+    constructor(point, opts){}
+
+
+}
